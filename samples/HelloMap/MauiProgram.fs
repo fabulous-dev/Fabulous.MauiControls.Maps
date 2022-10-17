2fbd9f1c799f7463586584c040051021095a77e6
Timothé Larivière <timothe.lariviere@outlook.com>
2022-10-17 16:31:52 +0200

    Working samples and CI

--- samples/HelloMap/MauiProgram.fs
+++ samples/HelloMap/MauiProgram.fs
@@ -2,12 +2,14 @@
 
 open Microsoft.Maui.Hosting
 open Fabulous.Maui
+open Fabulous.Maui.Maps
 
 type MauiProgram =
     static member CreateMauiApp() =
         MauiApp
             .CreateBuilder()
             .UseFabulousApp(App.program)
+            .UseFabulousMaps()
             .ConfigureFonts(fun fonts ->
                 fonts
                     .AddFont("OpenSans-Regular.ttf", "OpenSansRegular")
